408b9f6fbeff96c813b89680ffbddc532f537458
Patrick Nicholas <patricknicholas@foxmail.com>
2018-02-28 10:41:14 +0800

    [verify] add & pass FailAgree test

--- simu/verify/raft_agree_test.go
+++ simu/verify/raft_agree_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/thinkermao/bior/raft/core/conf"
 	"github.com/thinkermao/bior/simu/env"
+	"github.com/thinkermao/bior/simu/raft"
 )
 
 func TestRaft_BasicAgree(t *testing.T) {
@@ -29,6 +30,37 @@ func TestRaft_BasicAgree(t *testing.T) {
 			t.Fatalf("got index %v but expected %v", xindex, index)
 		}
 	}
+
+	fmt.Printf("  ... Passed\n")
+}
+
+func TestRaft_FailAgree(t *testing.T) {
+	servers := 3
+	env := envior.MakeEnvironment(t, servers, false)
+	defer env.Cleanup()
+
+	fmt.Printf("Test: agreement despite follower disconnection ...\n")
+
+	env.One(101, servers)
+
+	// follower network disconnection
+	leader := env.CheckOneLeader()
+	env.Disconnect((leader + 1) % servers)
+
+	// agree despite one disconnected server?
+	env.One(102, servers-1)
+	env.One(103, servers-1)
+	sleep(raft.ElectionTimeout)
+	env.One(104, servers-1)
+	env.One(105, servers-1)
+
+	// re-connect
+	env.Connect((leader + 1) % servers)
+
+	// agree with full set of servers?
+	env.One(106, servers)
+	sleep(raft.ElectionTimeout)
+	env.One(107, servers)
 
 	fmt.Printf("  ... Passed\n")
 }
